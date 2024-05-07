6d1e5dc339119ca99f050986455ac70429642beb
SenaProfeAlbeiro <earamos42@misena.edu.co>
2024-05-07 16:00:14 -0500

    Ejemplos Dart

---
+++ taller01_dart/02_ejemplos/04_const_final.dart
@@ -0,0 +1,21 @@
+void main() {
+  const CODIGO = "AR05";
+  String nombre = "Albeiro";
+  var apellido = "Ramos";
+  final DOCUMENTO = 1001053569;
+  String sexo = 'm';
+  int edad = 41;
+  double salario = 158323.15;
+  dynamic estatura = 1.72;
+  bool transporte = true;
+
+  print("Código:     $CODIGO");
+  print("Nombre:     $nombre");
+  print("Apellido:   $apellido");
+  print("Documento:  $DOCUMENTO");
+  print("Sexo:       $sexo");
+  print("Edad:       $edad");
+  print("Salario:    $salario");
+  print("Estatura:   $estatura");
+  print("Transporte: $transporte");
+}
