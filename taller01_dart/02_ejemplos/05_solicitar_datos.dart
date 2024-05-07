a4cfa71d9c5ed57e063dd63d6f9eaf6154d0107b
SenaProfeAlbeiro <earamos42@misena.edu.co>
2024-05-07 16:57:25 -0500

    Ejemplos Dart

---
+++ taller01_dart/02_ejemplos/05_solicitar_datos.dart
@@ -0,0 +1,48 @@
+import 'dart:io';
+
+void main() {
+  // Inicio del Algoritmo
+  print("\n" + ("-" * 50) + "\n");
+
+  // Declarar constantes y variables
+  const CODIGO = "AR05";
+  final DOCUMENTO = 1001053569;
+  var nombre;
+  var apellido;
+  var sexo;
+  var edad;
+  var salario;
+  var estatura;
+  var transporte;
+
+  // Entrada de Datos
+  stdout.write('Digite su nombre: \t');
+  nombre = stdin.readLineSync();
+  stdout.write('Digite su apellido: \t');
+  apellido = stdin.readLineSync();
+  stdout.write('Digite su sexo m/f: \t');
+  sexo = stdin.readLineSync();
+  stdout.write('Digite su edad: \t');
+  edad = stdin.readLineSync();
+  stdout.write('Digite su salario: \t');
+  salario = stdin.readLineSync();
+  stdout.write('Digite su estatura: \t');
+  estatura = stdin.readLineSync();
+  stdout.write('Digite su transporte: \t');
+  transporte = stdin.readLineSync();
+  print("\n" + ("-" * 50) + "\n");
+
+  // Salida de datos
+  print("Código:        $CODIGO");
+  print("Documento:     $DOCUMENTO");
+  print("Nombre:        $nombre");
+  print("Apellido:      $apellido");
+  print("Sexo:          $sexo");
+  print("Edad:          $edad");
+  print("Salario:       $salario");
+  print("Estatura:      $estatura");
+  print("Transporte:    $transporte");
+
+  // Fin del algoritmo
+  print("\n" + ("-" * 50) + "\n");
+}
