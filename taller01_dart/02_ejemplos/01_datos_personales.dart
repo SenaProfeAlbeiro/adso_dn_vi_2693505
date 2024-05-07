2ddc3d38f87145031f25dc93e913e8540a592205
SenaProfeAlbeiro <earamos42@misena.edu.co>
2024-05-07 15:15:53 -0500

    Ejemplos Dart

---
+++ taller01_dart/02_ejemplos/01_datos_personales.dart
@@ -0,0 +1,13 @@
+void main() {
+  String nombre = "Albeiro";
+  String sexo = 'm';
+  int edad = 41;
+  double salario = 158323.15;
+  bool transporte = true;
+
+  print(nombre);
+  print(sexo);
+  print(edad);
+  print(salario);
+  print(transporte);
+}
