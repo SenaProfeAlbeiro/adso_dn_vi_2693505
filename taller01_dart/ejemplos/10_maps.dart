492d31bc409f7d6c9822a80053f6b1c58e17b307
SenaProfeAlbeiro <earamos42@misena.edu.co>
2024-05-16 16:43:57 -0500

    Listas y Mapas

--- taller01_dart/ejemplos/10_maps.dart
+++ taller01_dart/ejemplos/10_maps.dart
@@ -1,8 +1,14 @@
 void main() {
-  /**
-   * Imprimir datos personales: nombre, regional, edad, 
-   * funcionario (true, false), altura (metros)
-   **/
+
+  // Mapa: Con la palabra reservada 'Map'
+  Map comidas = {    
+    'favorita'  : 'hamburguesas',
+    'agradables': 'sushi',
+    'tolerable' : 'postre'
+  };
+  print(comidas);
+  
+  // Mapa: Con var, llaves y "clave" : "valor"
   var datos = {
     "name" : "Albeiro Ramos",
     "regional" : "Distrito Capital",
@@ -10,6 +16,12 @@ void main() {
     "officer" : false,
     "height" : 1.72
   }; 
-
   print(datos);
+
+  // Mapa: Con 'var', 'Map()', corchetes[clave] = "valor"
+  var comidas02 = Map();
+  comidas02['favorita'] = 'hamburguesas';
+  comidas02['agradables'] = 'sushi';
+  comidas02['tolerable'] = 'postre';
+  print(comidas02);
 }
